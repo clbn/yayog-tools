17582eca21cce5bfd6955753b40cf76f23e2653d
Alex Olshansky <i@creagenics.com>
2013-07-16 22:30:31 +0400

    Initial commit

---
+++ ladder-sets.js
@@ -0,0 +1,11 @@
+﻿var initialRepSum = parseInt(process.argv[2], 10);
+var ladder = [1, 2, 3, 4, 3, 2];
+var set = 0;
+var repSum = 0;
+
+while (repSum < initialRepSum) {
+  var reps = ladder[set%6];
+  set++;
+  console.log('Set ' + set + ': ' + reps + ' reps');
+  repSum += reps;
+}
