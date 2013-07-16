d1f500f8fb5d47ae63cebc74a18e1fb44d812486
Alex Olshansky <i@creagenics.com>
2013-07-16 22:35:22 +0400

    Add blank lines and "Total"

--- ladder-sets.js
+++ ladder-sets.js
@@ -1,4 +1,4 @@
-﻿var initialRepSum = parseInt(process.argv[2], 10);
+var initialRepSum = parseInt(process.argv[2], 10);
 var ladder = [1, 2, 3, 4, 3, 2];
 var set = 0;
 var repSum = 0;
@@ -7,5 +7,10 @@ while (repSum < initialRepSum) {
   var reps = ladder[set%6];
   set++;
   console.log('Set ' + set + ': ' + reps + ' reps');
+  if (set%6 === 0) {
+    console.log('');
+  }
   repSum += reps;
 }
+
+console.log('\nTotal: ' + set + ' sets, ' + repSum + ' reps');
